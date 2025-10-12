ec41664c589179c617814d2085c860eca852bb07
andrerocker <andre.souza@gmail.com>
2025-10-12 14:11:39 -0300

    playing with logger

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
 mod marv;
 
+use env_logger;
+use log;
 use marv::plugins;
 use std::io::{self, BufReader, BufWriter, prelude::*};
 use std::net::TcpStream;
 
 fn plugins_enabled() -> Vec<Box<dyn plugins::Plugin>> {
     return vec![
-        Box::new(plugins::Logger{}),
-        Box::new(plugins::Login{}),
-        Box::new(plugins::Pong{}),
-        Box::new(plugins::Channel{}),
-        Box::new(plugins::Hello{}),
+        Box::new(plugins::Logger {}),
+        Box::new(plugins::Login {}),
+        Box::new(plugins::Pong {}),
+        Box::new(plugins::Channel {}),
+        Box::new(plugins::Hello {}),
     ];
 }
 
 fn main() -> io::Result<()> {
+    env_logger::init();
+
+    let server = "localhost:6667";
+    log::info!("Initializing marvbot - {}", server);
+
     let plugins = plugins_enabled();
-    let stream = TcpStream::connect("localhost:6667")?;
+    let stream = TcpStream::connect(server)?;
     let mut reader = BufReader::new(&stream);
     let mut writer = BufWriter::new(&stream);
     let mut protocol = String::new();
