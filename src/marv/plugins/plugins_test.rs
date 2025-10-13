339166fcabfebb6e995124d23c1ef519d6c63efb
andrerocker <andre.souza@gmail.com>
2025-10-12 22:43:14 -0300

    fixing broken specs

--- src/marv/plugins/plugins_test.rs
+++ src/marv/plugins/plugins_test.rs
@@ -2,7 +2,9 @@ use super::*;
 
 #[test]
 fn plugin_should_be_selected() {
-    let login = Login{};
+    let login = Login {
+        nickname: "malcolm".to_string(),
+    };
     let protocol = "Blach Blah! Could not resolve your hostname Bleh bleh".to_string();
 
     assert_eq!(login.is_enabled(&protocol), true);
@@ -10,7 +12,9 @@ fn plugin_should_be_selected() {
 
 #[test]
 fn plugin_shouldnt_be_selected() {
-    let login = Login{};
+    let login = Login {
+        nickname: "malcolm".to_string(),
+    };
     let protocol = "Blach Blah! Bleh bleh".to_string();
 
     assert_eq!(login.is_enabled(&protocol), false);
@@ -18,11 +22,13 @@ fn plugin_shouldnt_be_selected() {
 
 #[test]
 fn plugin_should_return_login_payload() {
-    let login = Login{};
+    let login = Login {
+        nickname: "malcolm".to_string(),
+    };
     let protocol = "".to_string();
     let response = vec![
-        "USER andrerocker * * :Andre\r\n".to_string(),
-        "NICK andrerocker\r\n".to_string(), 
+        "USER malcolm * * :malcolm\r\n".to_string(),
+        "NICK malcolm\r\n".to_string(),
     ];
 
     assert_eq!(login.perform(&protocol), response);
