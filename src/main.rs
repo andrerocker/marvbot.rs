f607f0e2fa21a5a835fe1015c6a582f155f88a8a
andrerocker <andre.souza@gmail.com>
2025-10-15 21:38:23 -0300

    comeback to old version xD

--- src/main.rs
+++ src/main.rs
@@ -10,52 +10,33 @@ use std::net::TcpStream;
 fn main() -> io::Result<()> {
     env_logger::init();
 
-    match config::read_configuration() {
-        Ok(setup) => {
-            let hostname = setup.config.hostname.clone();
-            log::info!("Initializing marvbot - {}", hostname);
+    let setup = config::read_configuration().unwrap();
+    let hostname = setup.config.hostname.clone();
+    log::info!("Initializing marvbot - {}", hostname);
 
-            match TcpStream::connect(&hostname) {
-                Ok(stream) => {
-                    let mut reader = BufReader::new(&stream);
-                    let mut writer = BufWriter::new(&stream);
+    let stream = TcpStream::connect(&hostname).unwrap();
+    let mut reader = BufReader::new(&stream);
+    let mut writer = BufWriter::new(&stream);
 
-                    let mut protocol = String::new();
-                    let mut plugins = plugins::default(&setup);
+    let mut protocol = String::new();
+    let mut plugins = plugins::default(&setup);
 
-                    loop {
-                        if let Ok(bytes_read) = reader.read_line(&mut protocol) {
-                            if bytes_read == 0 {
-                                break;
-                            }
+    loop {
+        if let Ok(bytes_read) = reader.read_line(&mut protocol) {
+            if bytes_read == 0 {
+                break;
+            }
 
-                            for plugin in plugins.iter_mut() {
-                                if plugin.is_enabled(&protocol) {
-                                    for result in plugin.perform(&protocol) {
-                                        writer.write_all(result.as_bytes())?;
-                                    }
-                                }
-                            }
-
-                            writer.flush()?;
-                            protocol.clear();
-                        }
+            for plugin in plugins.iter_mut() {
+                if plugin.is_enabled(&protocol) {
+                    for result in plugin.perform(&protocol) {
+                        writer.write_all(result.as_bytes())?;
                     }
                 }
-                Err(e) => {
-                    log::error!(
-                        "IRC Server: Fail trying to connect to: {}, {}",
-                        &hostname,
-                        e
-                    )
-                }
             }
-        }
-        Err(e) => {
-            log::error!(
-                "Config: Fail trying to load Marv.toml configuration file: {}",
-                e
-            )
+
+            writer.flush()?;
+            protocol.clear();
         }
     }
 
