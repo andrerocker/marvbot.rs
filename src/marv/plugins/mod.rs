0b81f16a40dbb736eb58a776c33e73822faebae7
andrerocker <andre.souza@gmail.com>
2025-10-19 14:34:23 -0300

    mapping custom metrics directly

--- src/marv/plugins/mod.rs
+++ src/marv/plugins/mod.rs
@@ -2,9 +2,20 @@ pub mod core;
 pub mod kafka;
 
 use kafka::{consumer::KafkaConsumer, producer::KafkaProducer};
+use lazy_static::lazy_static;
+use prometheus::{IntCounterVec, register_int_counter_vec};
 
 use super::config;
 use core::{channel::Channel, hello::Hello, log::Logger, login::Login, pong::Pong};
+
+lazy_static! {
+    static ref MARV_PLUGIN_HIT_COUNTER: IntCounterVec = register_int_counter_vec!(
+        "marv_plugin_hit_counter",
+        "Number of calls made to a plugin",
+        &["type"]
+    )
+    .unwrap();
+}
 
 pub trait Plugin {
     fn name(&self) -> String;
@@ -31,8 +42,10 @@ pub fn dispatch<F: FnMut(String)>(
 ) {
     for plugin in plugins.iter_mut() {
         if plugin.is_enabled(&protocol) {
-            // dispatch_counter.with_label_values(&["all"]).inc();
-            // dispatch_counter.with_label_values(&[&plugin.name()]).inc();
+            MARV_PLUGIN_HIT_COUNTER.with_label_values(&["all"]).inc();
+            MARV_PLUGIN_HIT_COUNTER
+                .with_label_values(&[&plugin.name()])
+                .inc();
 
             for result in plugin.perform(&protocol) {
                 callback(result);
