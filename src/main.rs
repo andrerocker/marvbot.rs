59e994f0e2dc52f141cd0fcb8bf12bf7ceccd81e
andrerocker <andre.souza@gmail.com>
2025-10-19 09:28:29 -0300

    extracting initialize to a custom function

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,27 @@ mod marv;
 use env_logger;
 use log;
 use marv::config;
+use marv::config::MarvSetup;
 use marv::plugins;
 use prometheus_exporter::prometheus::register_counter_vec;
 use prometheus_exporter::{self};
 use std::io::{self, BufReader, BufWriter, prelude::*};
 use std::net::TcpStream;
 
-fn initialize() {
+fn initialize() -> MarvSetup {
     env_logger::init();
-    let binding = "127.0.0.1:9184".parse().unwrap();
-    prometheus_exporter::start(binding).unwrap();
-}
+    prometheus_exporter::start("127.0.0.1:9184".parse().unwrap()).unwrap();
 
-fn main() -> io::Result<()> {
     let setup = config::read_configuration().unwrap();
     let hostname = setup.config.hostname.clone();
     log::info!("Initializing marvbot - {}", hostname);
 
-    let stream = TcpStream::connect(&hostname).unwrap();
+    return setup;
+}
+
+fn main() -> io::Result<()> {
+    let setup = initialize();
+    let stream = TcpStream::connect(&setup.config.hostname).unwrap();
     let mut reader = BufReader::new(&stream);
     let mut writer = BufWriter::new(&stream);
 
