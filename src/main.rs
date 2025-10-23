7851990cbb76332314ad7a8e94979833d2887e9d
andrerocker <andre.souza@gmail.com>
2025-10-23 20:26:13 -0300

    adding initial struct to play with diesel

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 mod marv;
 
+use diesel::Connection;
+use diesel::PgConnection;
+use dotenv::dotenv;
 use env_logger;
 use log;
 use marv::config;
@@ -7,11 +10,21 @@ use marv::config::MarvSetup;
 use marv::network;
 use marv::plugins;
 use prometheus_exporter::{self};
+use std::env;
 use std::io::{self, prelude::*};
 
+pub fn establish_connection() -> PgConnection {
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    PgConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
 fn initialize() -> MarvSetup {
+    dotenv().ok();
     env_logger::init();
     prometheus_exporter::start("127.0.0.1:9184".parse().unwrap()).unwrap();
+    establish_connection();
 
     let setup = config::read_configuration().unwrap();
     let hostname = setup.config.hostname.clone();
