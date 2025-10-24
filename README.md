0cb1f7891a80837e3e54663cd9e99847cb3d9307
andrerocker <andre.souza@gmail.com>
2025-10-24 13:16:51 -0300

    adding database plugin

--- README.md
+++ README.md
@@ -29,7 +29,8 @@ Your rust Awesome IRC Bot, it's a personal project mainly used to play with Rust
 - [ ] build
 - [ ] cross platform build (arm, x86, windows, linux, macos)
 - [ ] dotenv
-- [ ] singleton
+- [ ] singleton (lazy_static?)
+- [ ] argparse
 
 ### Plugins
 
---
+++ src/marv/plugins/etc/database.rs
@@ -0,0 +1,73 @@
+use std::env;
+
+use crate::marv::models::*;
+use crate::marv::schema::messages;
+use crate::marv::{config::MarvSetup, plugins::Plugin};
+use diesel::Connection;
+use diesel::PgConnection;
+use diesel::prelude::*;
+
+pub struct Database {
+    pub connection: PgConnection,
+}
+
+impl Database {
+    pub fn new(_setup: &MarvSetup) -> Box<dyn Plugin> {
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+        let connection = PgConnection::establish(&database_url)
+            .unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
+
+        Box::new(Database {
+            connection: connection,
+        })
+    }
+
+    pub fn messages(&mut self) {
+        use crate::marv::schema::messages::dsl::*;
+        let connection = &mut self.connection;
+
+        let results = messages
+            .filter(published.eq(true))
+            .select(Message::as_select())
+            .load(connection)
+            .expect("Error loading messages");
+
+        println!("Displaying {} messages", results.len());
+        for post in results {
+            println!("{}", post.title);
+            println!("-----------\n");
+            println!("{}", post.body);
+        }
+    }
+
+    pub fn create(&mut self, message: &String) -> Message {
+        let title = "Acme";
+        let body = &message;
+
+        let new_post = NewMessage { title, body };
+        let connection = &mut self.connection;
+
+        diesel::insert_into(messages::table)
+            .values(&new_post)
+            .returning(Message::as_returning())
+            .get_result(connection)
+            .expect("Error saving new post")
+    }
+}
+
+impl Plugin for Database {
+    fn name(&self) -> String {
+        return "Database".to_string();
+    }
+
+    fn is_enabled(&self, _message: &String) -> bool {
+        return true;
+    }
+
+    fn perform(&mut self, message: &String) -> Vec<String> {
+        self.create(message);
+
+        return vec![];
+    }
+}
