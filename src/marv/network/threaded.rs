d361bb2381131462df81913a11bb7389c369fc24
andrerocker <andre.souza@gmail.com>
2025-11-03 13:23:56 -0300

    thread: draft fn to start work with threads

--- src/marv/network/threaded.rs
+++ src/marv/network/threaded.rs
@@ -3,20 +3,31 @@ use log::info;
 use std::{
     io::{self, BufReader, BufWriter, prelude::*},
     net::TcpStream,
-    sync::mpsc,
+    sync::mpsc::{self, Receiver, Sender},
     thread::{self, JoinHandle},
 };
 
-pub fn stream<F: FnMut(&mut BufWriter<&TcpStream>, &String)>(
-    setup: MarvSetup,
-) -> io::Result<JoinHandle<io::Result<()>>> {
+pub fn stream(setup: MarvSetup) -> io::Result<()> {
     let stream = TcpStream::connect(&setup.config.hostname)?;
-    // let mut writer = BufWriter::new(&stream);
-    let (writer, rx) = mpsc::channel::<String>();
+    let mut reader = BufReader::new(&stream);
+    let mut writer = BufWriter::new(&stream);
 
-    let thread = thread::spawn(move || {
+    let (sender, receiver) = mpsc::channel::<String>();
+    let reader_thread = reader_thread(&reader, sender);
+    let writer_thread = writer_thread(&writer, receiver);
+
+    reader_thread.join();
+    writer_thread.join();
+
+    Ok(())
+}
+
+fn reader_thread(
+    reader: &BufReader<&TcpStream>,
+    sender: Sender<String>,
+) -> JoinHandle<io::Result<()>> {
+    thread::spawn(|| {
         let mut protocol = String::new();
-        let mut reader = BufReader::new(&stream);
 
         loop {
             if let Ok(bytes_read) = reader.read_line(&mut protocol) {
@@ -33,7 +44,15 @@ pub fn stream<F: FnMut(&mut BufWriter<&TcpStream>, &String)>(
                 protocol.clear();
             }
         }
-    });
+    })
+}
 
-    Ok(thread)
+fn writer_thread(
+    writer: &BufWriter<&TcpStream>,
+    receiver: Receiver<String>,
+) -> JoinHandle<io::Result<()>> {
+    thread::spawn(|| match receiver.recv() {
+        Ok(received) => writer.write(received.as_bytes()),
+        Err(error) => log::error!("Problems to pick messagem: {}", error),
+    })
 }
