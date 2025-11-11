089a0ea44dc1490dde6cb308bab3b493f25e43c8
andrerocker <andre.souza@gmail.com>
2025-11-11 18:47:24 -0300

    engine: keep only the evented implementation

--- src/marv/engine.rs
+++ src/marv/engine.rs
@@ -1,3 +1,50 @@
-pub mod event;
-pub mod single;
-pub mod threaded;
+use std::io::Result;
+
+use crate::marv::config;
+use crate::marv::plugins;
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter},
+    net::TcpSocket,
+};
+
+#[tokio::main]
+pub async fn start() -> Result<()> {
+    let config = &config::MARV.config;
+
+    let addr = config.hostname.clone().parse().unwrap();
+    let socket = TcpSocket::new_v4()?;
+
+    let stream = socket.connect(addr).await?;
+    let (reader, writer) = stream.into_split();
+
+    let mut reader = BufReader::new(reader);
+    let mut writer = BufWriter::new(writer);
+    let mut protocol = String::new();
+
+    let mut plugins = plugins::default();
+
+    loop {
+        if let Ok(bytes_read) = reader.read_line(&mut protocol).await {
+            if bytes_read == 0 {
+                log::error!("Problems trying to read from the network (connection closed)");
+                break;
+            }
+
+            for result in plugins::dispatch(&mut plugins, &protocol).await? {
+                if let Err(error) = writer.write_all(result.as_bytes()).await {
+                    log::error!("Problems trying to write to the network: {}", error);
+                    break;
+                };
+            }
+
+            if let Err(error) = writer.flush().await {
+                log::error!("Problems trying to flush data to the network: {}", error);
+                break;
+            }
+
+            protocol.clear();
+        }
+    }
+
+    Ok(())
+}
