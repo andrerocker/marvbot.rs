293c1b072fb0653b2b8742acbe6e169938234456
andrerocker <andre.souza@gmail.com>
2025-12-08 11:56:58 -0300

    plugins: now is possible to add custom plugins at initialization

--- marv_core/src/main.rs
+++ marv_core/src/main.rs
@@ -1,9 +1,29 @@
 mod marv;
-use marv::engine;
+use marv::{
+    engine,
+    plugins::{
+        self,
+        core::{hello_fast::HelloFast, hello_slow::HelloSlow, spam::Spam},
+    },
+};
+use marv_plugins::{
+    ask_chatgpt::AskChatGPT,
+    kafka::{consumer::KafkaConsumer, producer::KafkaProducer},
+    todo::Todo,
+};
 
 #[tokio::main]
 pub async fn main() {
     engine::initialize().await;
+    plugins::initialize(vec![
+        KafkaProducer::new(),
+        KafkaConsumer::new(),
+        Todo::new(),
+        HelloFast::new(),
+        HelloSlow::new(),
+        AskChatGPT::new(),
+        Spam::new(),
+    ]);
 
     match engine::execute().await {
         Ok(_) => log::info!("Bye :D"),
--- marv_core/src/marv/plugins.rs
+++ marv_core/src/marv/plugins.rs
@@ -2,64 +2,38 @@ pub mod core;
 pub mod dispatch;
 pub mod scheduled;
 
-use core::{
-    channel::Channel, hello::Hello, hello_fast::HelloFast, hello_slow::HelloSlow, log::Logger,
-    login::Login, pong::Pong, spam::Spam,
-};
-use marv_api::plugins::{DynamicPlugin, DynamicPluginVec};
-use marv_plugins::{
-    ask_chatgpt::AskChatGPT,
-    kafka::{consumer::KafkaConsumer, producer::KafkaProducer},
-    todo::Todo,
-};
+use core::{channel::Channel, hello::Hello, log::Logger, login::Login, pong::Pong};
+use marv_api::plugins::{DynamicPluginVec, Plugin};
 use once_cell::sync::OnceCell;
 
 static PLUGINS: OnceCell<DynamicPluginVec> = OnceCell::new();
 
-fn default_plugins() -> &'static DynamicPluginVec {
-    PLUGINS.get_or_init(|| {
-        vec![
-            Logger::new(),
-            Login::new(),
-            Pong::new(),
-            Channel::new(),
-            Hello::new(),
-            KafkaProducer::new(),
-            KafkaConsumer::new(),
-            Todo::new(),
-            HelloFast::new(),
-            HelloSlow::new(),
-            AskChatGPT::new(),
-            Spam::new(),
-        ]
-    })
+pub fn initialize(custom: DynamicPluginVec) {
+    let mut default = vec![
+        Logger::new(),
+        Login::new(),
+        Pong::new(),
+        Channel::new(),
+        Hello::new(),
+    ];
+
+    default.extend(custom);
+    PLUGINS.set(default);
 }
 
-fn default_schedulables() -> Vec<(String, &'static DynamicPlugin)> {
-    let mut candidates = Vec::new();
+fn default_plugins() -> &'static DynamicPluginVec {
+    PLUGINS.get().unwrap()
+}
 
-    for plugin in crate::marv::plugins::default_plugins() {
-        if let Some(schedulable) = plugin.schedule() {
-            candidates.push((schedulable, plugin));
+fn default_schedulables() -> Vec<(String, &'static Box<dyn Plugin>)> {
+    let mut candidates: Vec<(String, &'static Box<dyn Plugin>)> = Vec::new();
+    let plugins = default_plugins();
+
+    for plugin in plugins.iter() {
+        if let Some(appointment) = plugin.schedule() {
+            candidates.push((appointment, plugin));
         }
     }
 
     candidates
 }
-
-// #[test]
-// fn test_default_plugins() -> Result<(), Box<dyn std::error::Error>> {
-//     let plugins = &PLUGINS;
-//     let detect = |name: &str| plugins.iter().find(|p| p.name() == name);
-
-//     assert!(detect("Logger").is_some());
-//     assert!(detect("Login").is_some());
-//     assert!(detect("Pong").is_some());
-//     assert!(detect("Channel").is_some());
-//     assert!(detect("Hello").is_some());
-//     assert!(detect("KafkaProducer").is_some());
-//     assert!(detect("KafkaConsumer").is_some());
-//     assert!(detect("Todo").is_some());
-
-//     Ok(())
-// }
